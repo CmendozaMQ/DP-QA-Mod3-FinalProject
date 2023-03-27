c802f9dc1740fa73b3bcf74c22bb7814e70177e9
CmendozaMQ <csar.mendoza@outlook.com>
2023-03-26 20:19:29 -0400

    upload files

---
+++ config.app.ts
@@ -0,0 +1,7 @@
+export const TESTDATA = {
+    email: '',
+    password: '',
+    url: 'https://encuentralos.org/',
+    apiUrl: 'https://api.todoist.com/rest/v2/',
+    apiToken: 'd574c2359cb59a80ac4fbfa4a2fcdf38a58cf06e',
+}
